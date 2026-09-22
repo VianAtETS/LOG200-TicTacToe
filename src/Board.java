03947715bd5d9de3b84b5ed1a79485a61ce6bf43
Vianpyro <vianney+github@veremme.org>
2026-09-22 14:33:29 +0000

    feat(board): implement play method to place mark on the board

--- src/Board.java
+++ src/Board.java
@@ -13,7 +13,9 @@ class Board {
 
     // Place la pièce 'mark' sur le plateau, à la
     // position spécifiée dans Move
-    public void play(Move m, Mark mark) {}
+    public void play(Move m, Mark mark) {
+        board[m.getRow()][m.getCol()] = mark;
+    }
 
     // retourne  100 pour une victoire
     //          -100 pour une défaite
