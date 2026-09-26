55832f8cb4aeebef5b2c30f7f679063fa17fd525
Yacine Benchoubane <benchoubaneyacine@gmail.com>
2026-09-26 11:42:44 -0400

    Implemented MiniMax

--- src/CPUPlayer.java
+++ src/CPUPlayer.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 // IMPORTANT: Il ne faut pas changer la signature des méthodes
 // de cette classe, ni le nom de la classe.
-// Vous pouvez par contre ajouter d'autres méthodes (ça devrait 
+// Vous pouvez par contre ajouter d'autres méthodes (ça devrait
 // être le cas)
 class CPUPlayer
 {
@@ -13,11 +13,17 @@ class CPUPlayer
     // au début de votre MinMax ou Alpha Beta.
     private int numExploredNodes;
     private Mark cpu;
+    private Mark cpu2;
     // Le constructeur reçoit en paramètre le
     // joueur MAX (X ou O)
     public CPUPlayer(Mark cpu){
         this.numExploredNodes=0;
         this.cpu=cpu;
+        if(cpu==Mark.O){
+            this.cpu2=Mark.X;
+        }else{
+            this.cpu2=Mark.O;
+        }
     }
 
     // Ne pas changer cette méthode
@@ -29,9 +35,23 @@ class CPUPlayer
     // plusieurs coups possibles si et seuleument si plusieurs coups
     // ont le même score.
     public ArrayList<Move> getNextMoveMinMax(Board board)
-    {   
-        numExploredNodes++;
+    {
         ArrayList<Move> nextMoveMM = new ArrayList<>();
+        ArrayList<Move> moves = board.availablePos();
+        int bestScore = Integer.MIN_VALUE;
+        for(int i=0;i<moves.size();i++){
+            Move m = moves.get(i);
+            board.play(m, cpu);
+            int val = minmax(board, false);
+            board.undoMove(m);
+            if(val>bestScore){
+                bestScore=val;
+                nextMoveMM.clear();
+                nextMoveMM.add(m);
+            }else if(val==bestScore){
+                nextMoveMM.add(m);
+            }
+        }
         return nextMoveMM;
 
     }
@@ -46,4 +66,42 @@ class CPUPlayer
 
     }
 
+    //le minmax
+    private int minmax(Board board,boolean maximizing){
+        numExploredNodes++;
+        int score =board.evaluate(cpu);
+        ArrayList<Move> moves = board.availablePos();
+        if(score==100){
+            return score;
+        }else if(score==-100){
+            return score;
+        }else if(moves.isEmpty()){
+            return 0;
+        }
+        if(maximizing){
+            int bestScore = Integer.MIN_VALUE;
+            for(int i=0;i<moves.size();i++){
+                Move m = moves.get(i);
+                board.play(m, cpu);
+                int val = minmax(board, !maximizing);
+                board.undoMove(m);
+                if(val>bestScore){
+                    bestScore=val;
+                }
+            }
+            return bestScore;
+        }else{
+            int leastScore = Integer.MAX_VALUE;
+            for(int i=0;i<moves.size();i++){
+                Move m = moves.get(i);
+                board.play(m, cpu2);
+                int val = minmax(board, !maximizing);
+                board.undoMove(m);
+                if(leastScore>val){
+                    leastScore = val;
+                }
+            }
+            return leastScore;
+        }
+    }
 }
